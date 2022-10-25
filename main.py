e25c2e39aa50f6517963c56a00b9f686d06d83fb
Aleksei Kudinov <reanim86@yandex.ru>
2022-10-26 00:31:39 +0300

    Add func add_data

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import json
+
 import sqlalchemy
 import sqlalchemy as sq
-from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
 
 Base = declarative_base()
 
@@ -19,16 +21,69 @@ class Book(Base):
 
     publisher = relationship(Publisher, backref='book')
 
+class Shop(Base):
+    __tablename__ = 'shop'
 
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(length=40), nullable=False)
+
+class Stock(Base):
+    __tablename__ = 'stock'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    count = sq.Column(sq.Integer)
+    id_book = sq.Column(sq.Integer, sq.ForeignKey('book.id'), nullable=False)
+    id_shop = sq.Column(sq.Integer, sq.ForeignKey('shop.id'), nullable=False)
+
+    book = relationship(Book, backref='stock')
+    shop = relationship(Shop, backref='stock')
+
+class Sale(Base):
+    __tablename__ = 'sale'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    price = sq.Column(sq.Numeric, nullable=False)
+    date_sale = sq.Column(sq.Date, nullable=False)
+    count = sq.Column(sq.Integer, nullable=False)
+    id_stock = sq.Column(sq.Integer, sq.ForeignKey('stock.id'), nullable=False)
+
+    stock = relationship(Stock, backref='sale')
 
 def create_tables(engine):
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
+
+def add_data():
+    """
+    Функция читает json файл и записывает данные из него в БД
+    """
+    with open('fixtures/tests_data.json') as f:
+        data = json.load(f)
+    for record in data:
+        model = {
+            'publisher': Publisher,
+            'shop': Shop,
+            'book': Book,
+            'stock': Stock,
+            'sale': Sale,
+        }[record.get('model')]
+        session.add(model(id=record['pk'], **record['fields']))
+    return
 
 login = 'postgres'
 password = 'Tehn89tehn'
 base = 'db6'
 DSN = f'postgresql://{login}:{password}@localhost:5432/{base}'
 engine = sqlalchemy.create_engine(DSN)
+
 create_tables(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+add_data()
+session.commit()
+
+
+
+
 
